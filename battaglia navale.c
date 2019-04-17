7abedac8ca68b6caa93c7416e3ff76ab890a138b
Simone Cimolato <Simonecimolato@users.noreply.github.com>
2019-04-17 12:51:43 +0200

    Update battaglia navale.c

--- battaglia navale.c
+++ battaglia navale.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define DIM 10
 
-generateCPShips();
+void generateShips(int, int);
 
-typedef struct s_nave{
-    int lunghezza;
-    bool orientamento;
-    int posX;
-    int posY;
-}nave;
+bool computerBoard[DIM][DIM];
+bool playerBoard[DIM][DIM];
 
-nave computerBoard[DIM][DIM];
-nave playerBoard[DIM][DIM];
-
-int main(){
-
-
-
-
-return 0;
-}
-
-
-void generateCPShips(){
-    int i;
-    for(i = 0; i <){
-
-
-
+int main()
+{
+    // cinque = 1, quattro = 2, tre = 3, due = 2;
+    int i=0, j;
+    for(i=0; i<DIM;i++){
+        for(j=0;j<DIM;j++){
+            computerBoard[i][j] = 0;
+        }
     }
 
 
@@ -37,13 +24,70 @@ void generateCPShips(){
 
 
 
+
+
+
+
+    generateShips(1, 5);
+    generateShips(2, 4);
+    generateShips(3, 3);
+    generateShips(2, 2);
+
+    printf("SALVE, BENVENUTO SU BATTAGLIA NAVALE: \n");
+    //printf("");
+
+    for(i=0;i<DIM;i++){
+        for(j=0;j<DIM;j++){
+            if(computerBoard[i][j] == 0){
+                    printf("0 ");
+            }
+            else printf("1 ");
+        }
+        printf("\n\n");
+    }
+
 }
 
 
+void generateShips(int numeroNavi, int dimensione){
+    int k;
+    for(k=0; k<numeroNavi; k++){
+        int casuale = rand() % 2;
+        if (casuale == 0){
+            //orizzontale
 
+            int colonnaT = rand() % dimensione;
+            int rigaT = rand() % 10;
+            int i;
 
+            for(i=0;i<dimensione;i++){
+                if(computerBoard[rigaT][colonnaT+i] == 1){
+                    generateShips(numeroNavi, dimensione);
+                }
+            }
 
+            for(i=0; i<dimensione;i++){
+                computerBoard[rigaT][colonnaT + i] = 1;
+            }
+        }
+        else {
+            //verticale
+            int colonnaT = rand() % 10;
+            int rigaT = rand() % dimensione;
+            printf("%d", rigaT);
+            printf("%d", colonnaT);
 
+            int i;
 
+            for(i=0;i<dimensione;i++){
+                if(computerBoard[rigaT + i][colonnaT] == 1){
+                    generateShips(numeroNavi, dimensione);
+                }
+            }
 
-
+            for(i=0; i<dimensione;i++){
+                computerBoard[rigaT + i][colonnaT] = 1;
+            }
+        }
+    }
+}
